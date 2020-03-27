73df72dd1274bde29c235c3a1140d56ac1b00b3e
dev-dtech <deep64222@gmail.com>
2020-03-27 13:46:50 +0530

    update

--- public/script.js
+++ public/script.js
@@ -1,7 +1,4 @@
-
-
 function escapeHtml(text) {
-
 	return text
 		.replace(/&/g, "&amp;")
 		.replace(/</g, "&lt;")
@@ -9,7 +6,6 @@ function escapeHtml(text) {
 		.replace(/"/g, "&quot;")
 		.replace(/'/g, "&#039;");
 }
-
 
 let socket = io(),
 	chat_box = document.getElementById("chat-box"),
@@ -19,11 +15,11 @@ let socket = io(),
 	user = {
 		sender: {
 			name:
-			// Math.random()
-			prompt(
-				"Enter the name that you want every one to know you with",
-				"Name"
-			)
+				// Math.random()
+				prompt(
+					"Enter the name that you want every one to know you with",
+					"Name"
+				)
 		},
 
 		profile_image: "img.png"
@@ -31,7 +27,7 @@ let socket = io(),
 
 let lastchatdata, lastchatelement;
 
-let notification_audio=document.getElementById("notification-audio")
+let notification_audio = document.getElementById("notification-audio");
 
 console.log(user.sender.name);
 
@@ -45,71 +41,78 @@ var scrollToBottom = function() {
 	window.scrollTo(0, document.body.scrollHeight);
 };
 
-socket.on("receive-chat-history", data => {
-	chat_box.innerHTML = "";
-	data.forEach(chat => {
-		//message
-		if (chat.message) {
-			if (lastchatelement && lastchatdata.sender.name == chat.sender.name) {
-				lastchatelement.innerHTML += `
-            <div class="chat-message">${chat.message}</div>
-            `;
-			} else {
-				chat_box.innerHTML += `
-        <div class="chat" ${
-					chat.sender.name == user.sender.name ? 'id="you-chat"' : ""
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log("Name: " + profile.getName());
+	console.log("Image URL: " + profile.getImageUrl());
+	console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+	socket.on("receive-chat-history", data => {
+		chat_box.innerHTML = "";
+		data.forEach(chat => {
+			//message
+			if (chat.message) {
+				if (lastchatelement && lastchatdata.sender.name == chat.sender.name) {
+					lastchatelement.innerHTML += `
+				<div class="chat-message">${chat.message}</div>
+				`;
+				} else {
+					chat_box.innerHTML += `
+			<div class="chat" ${
+				chat.sender.name == user.sender.name ? 'id="you-chat"' : ""
+			}>
+				<div class="sender" ${
+					chat.sender.name == user.sender.name ? 'id="you-sender"' : ""
 				}>
-            <div class="sender" ${
-							chat.sender.name == user.sender.name ? 'id="you-sender"' : ""
-						}>
-                <div class="sender-name">${chat.sender.name}</div>
-                <img src="${chat.sender.image}" alt="" class="sender-image" />
-            </div>
-            <div class="chat-message">${chat.message}</div>
-        </div>
-        `;
+					<div class="sender-name">${chat.sender.name}</div>
+					<img src="${chat.sender.image}" alt="" class="sender-image" />
+				</div>
+				<div class="chat-message">${chat.message}</div>
+			</div>
+			`;
+				}
+				scrollToBottom();
+				updatelastchat(chat);
 			}
-			scrollToBottom();
-			updatelastchat(chat);
-		}
-		//file
-		// else if (chat.file_name) {
-		// 	if (lastchatelement && lastchatdata.sender.name == chat.sender.name) {
-		// 		lastchatelement.innerHTML += `
-		// 	<a href="./uploads/${chat.file_name}" class="chat-message" download>${chat.file_name}</a>
-        //     `;
-		// 	} else {
-		// 		chat_box.innerHTML += `
-        // <div class="chat" ${
-		// 			chat.sender.name == user.sender.name ? 'id="you-chat"' : ""
-		// 		}>
-        //     <div class="sender" ${
-		// 					chat.sender.name == user.sender.name ? 'id="you-sender"' : ""
-		// 				}>
-        //         <div class="sender-name">${chat.sender.name}</div>
-        //         <img src="${chat.sender.image}" alt="" class="sender-image" />
-        //     </div>
-		// 	P<a href="./uploads/${chat.file_name}" class="chat-message" download>${
-		// 			chat.file_name
-		// 		}</a>
-        // </div>
-        // `;
-		// 	}
-		// 	scrollToBottom();
-		// 	updatelastchat(chat);
-		// }
+			//file
+			// else if (chat.file_name) {
+			// 	if (lastchatelement && lastchatdata.sender.name == chat.sender.name) {
+			// 		lastchatelement.innerHTML += `
+			// 	<a href="./uploads/${chat.file_name}" class="chat-message" download>${chat.file_name}</a>
+			//     `;
+			// 	} else {
+			// 		chat_box.innerHTML += `
+			// <div class="chat" ${
+			// 			chat.sender.name == user.sender.name ? 'id="you-chat"' : ""
+			// 		}>
+			//     <div class="sender" ${
+			// 					chat.sender.name == user.sender.name ? 'id="you-sender"' : ""
+			// 				}>
+			//         <div class="sender-name">${chat.sender.name}</div>
+			//         <img src="${chat.sender.image}" alt="" class="sender-image" />
+			//     </div>
+			// 	P<a href="./uploads/${chat.file_name}" class="chat-message" download>${
+			// 			chat.file_name
+			// 		}</a>
+			// </div>
+			// `;
+			// 	}
+			// 	scrollToBottom();
+			// 	updatelastchat(chat);
+			// }
+		});
 	});
-});
 
-//message-receive
+	//message-receive
 
-socket.on("receive-chat", data => {
-	if (lastchatelement && lastchatdata.sender.name == data.sender.name) {
-		lastchatelement.innerHTML += `
+	socket.on("receive-chat", data => {
+		if (lastchatelement && lastchatdata.sender.name == data.sender.name) {
+			lastchatelement.innerHTML += `
         <div class="chat-message">${data.message}</div>
         `;
-	} else {
-		chat_box.innerHTML += `
+		} else {
+			chat_box.innerHTML += `
     <div class="chat">
         <div class="sender">
             <img src="${data.sender.image}" alt="" class="sender-image" />
@@ -118,24 +121,24 @@ socket.on("receive-chat", data => {
         <div class="chat-message">${data.message}</div>
     </div>
     `;
-	}
-	notification_audio.play();
-	scrollToBottom();
-	updatelastchat(data);
-	// console.log(data);
-});
+		}
+		notification_audio.play();
+		scrollToBottom();
+		updatelastchat(data);
+		// console.log(data);
+	});
 
-form.addEventListener("submit", e => {
-	e.preventDefault();
+	form.addEventListener("submit", e => {
+		e.preventDefault();
 
-	var message = escapeHtml(text.value);
+		var message = escapeHtml(text.value);
 
-	if (lastchatelement && lastchatdata.sender.name == user.sender.name) {
-		lastchatelement.innerHTML += `
+		if (lastchatelement && lastchatdata.sender.name == user.sender.name) {
+			lastchatelement.innerHTML += `
         <div class="chat-message">${message}</div>
         `;
-	} else {
-		chat_box.innerHTML += `
+		} else {
+			chat_box.innerHTML += `
         <div class="chat" id="you-chat">
             <div class="sender" id="you-sender">
                 <img src="img.png" alt="" class="sender-image" />
@@ -144,40 +147,40 @@ form.addEventListener("submit", e => {
             <div class="chat-message">${message}</div>
         </div>
         `;
-	}
-	scrollToBottom();
-	updatelastchat(user);
-	socket.emit("send-chat", {
-		sender: {
-			name: user.sender.name,
-			image: user.profile_image
-		},
-		message: message
+		}
+		scrollToBottom();
+		updatelastchat(user);
+		socket.emit("send-chat", {
+			sender: {
+				name: user.sender.name,
+				image: user.profile_image
+			},
+			message: message
+		});
+		text.value = "";
 	});
-	text.value = "";
-});
 
-var uploader = new SocketIOFileUpload(socket);
-let file_upload = document.getElementById("file-upload");
-let file_upload_button = document.getElementById("file-upload-button");
-let file_upload_form = document.getElementById("file-upload-form");
+	var uploader = new SocketIOFileUpload(socket);
+	let file_upload = document.getElementById("file-upload");
+	let file_upload_button = document.getElementById("file-upload-button");
+	let file_upload_form = document.getElementById("file-upload-form");
 
-uploader.listenOnDrop(document.getElementById("file_drop"));
+	uploader.listenOnDrop(document.getElementById("file_drop"));
 
-uploader.listenOnInput(file_upload);
+	uploader.listenOnInput(file_upload);
 
-uploader.addEventListener("progress", e => {
-	console.log((e.bytesLoaded / e.file.size) * 100);
-});
-uploader.addEventListener("complete", e => {
-	console.log("Upload complete");
-	e1 = e;
-});
-file_upload_button.addEventListener("click", e => {
-	if (e1) {
-		console.log(e1.file.name);
-		if (lastchatelement && lastchatdata.sender.name == user.sender.name) {
-			lastchatelement.innerHTML += `
+	uploader.addEventListener("progress", e => {
+		console.log((e.bytesLoaded / e.file.size) * 100);
+	});
+	uploader.addEventListener("complete", e => {
+		console.log("Upload complete");
+		e1 = e;
+	});
+	file_upload_button.addEventListener("click", e => {
+		if (e1) {
+			console.log(e1.file.name);
+			if (lastchatelement && lastchatdata.sender.name == user.sender.name) {
+				lastchatelement.innerHTML += `
 			<div class="chat-message">
 				<a id="file-link" href="./uploads/${e1.file.name}">
 					<img src="./style/file-logo.svg" alt="" srcset="" />
@@ -190,8 +193,8 @@ file_upload_button.addEventListener("click", e => {
 				<a href="./uploads/${e1.file.name}" id="download-button" download>Download</a>
 			</div>
 			`;
-		} else {
-			chat_box.innerHTML += `
+			} else {
+				chat_box.innerHTML += `
 			<div class="chat" id="you-chat">
 				<div class="sender" id="you-sender">
 					<img src="img.png" alt="" class="sender-image" />
@@ -210,15 +213,15 @@ file_upload_button.addEventListener("click", e => {
 				</div>
 			</div>
 			`;
-		}
-		scrollToBottom();
-		updatelastchat(user);
-		socket.emit("send-file", {
-			sender: {
-				name: user.sender.name,
-				image: user.profile_image
-			},
-			message: `
+			}
+			scrollToBottom();
+			updatelastchat(user);
+			socket.emit("send-file", {
+				sender: {
+					name: user.sender.name,
+					image: user.profile_image
+				},
+				message: `
 			<a id="file-link" href="./uploads/${e1.file.name}">
 				<img src="./style/file-logo.svg" alt="" srcset="" />
 				<br>
@@ -229,42 +232,42 @@ file_upload_button.addEventListener("click", e => {
 			<br>
 			<a href="./uploads/${e1.file.name}" id="download-button" download>Download</a>		
 			`
-		});
-	}
-});
-// socket.on("receive-file", data => {
-// 	if (lastchatelement && lastchatdata.sender.name == data.sender.name) {
-// 		lastchatelement.innerHTML += `
-// 		<a href="./uploads/${data.file}" class="chat-message" download>${data.file_name}</a>
-// 		`;
-// 	} else {
-// 		chat_box.innerHTML += `
-// 		<div class="chat">
-// 			<div class="sender">
-// 				<img src="img.png" alt="" class="sender-image" />
-// 				<div class="sender-name">${data.sender.name}</div>
-// 			</div>
-// 				<a href="./uploads/${data.file_name}" class="chat-message" download>${data.file_name}</a>
-				
-// 		</div>
-// 		`;
-// 	}
-// 	scrollToBottom();
-// 	updatelastchat(data);
-// });
+			});
+		}
+	});
+	// socket.on("receive-file", data => {
+	// 	if (lastchatelement && lastchatdata.sender.name == data.sender.name) {
+	// 		lastchatelement.innerHTML += `
+	// 		<a href="./uploads/${data.file}" class="chat-message" download>${data.file_name}</a>
+	// 		`;
+	// 	} else {
+	// 		chat_box.innerHTML += `
+	// 		<div class="chat">
+	// 			<div class="sender">
+	// 				<img src="img.png" alt="" class="sender-image" />
+	// 				<div class="sender-name">${data.sender.name}</div>
+	// 			</div>
+	// 				<a href="./uploads/${data.file_name}" class="chat-message" download>${data.file_name}</a>
 
-///Youtube///
+	// 		</div>
+	// 		`;
+	// 	}
+	// 	scrollToBottom();
+	// 	updatelastchat(data);
+	// });
 
-let youtube_upload=document.getElementById("youtube-upload");
-function upload_youtube(){
-	if (lastchatelement && lastchatdata.sender.name == user.sender.name) {
-		lastchatelement.innerHTML += `
+	///Youtube///
+
+	let youtube_upload = document.getElementById("youtube-upload");
+	function upload_youtube() {
+		if (lastchatelement && lastchatdata.sender.name == user.sender.name) {
+			lastchatelement.innerHTML += `
 		<div class="chat-message">
 			<iframe id="youtube" src="${youtube_upload.value}" allowfullscreen></iframe>
 		</div>
         `;
-	} else {
-		chat_box.innerHTML += `
+		} else {
+			chat_box.innerHTML += `
         <div class="chat" id="you-chat">
             <div class="sender" id="you-sender">
                 <img src="img.png" alt="" class="sender-image" />
@@ -275,18 +278,32 @@ function upload_youtube(){
 			</div>
         </div>
         `;
-	}
-	scrollToBottom();
-	updatelastchat(user);
-	socket.emit("send-chat",{
-		sender: {
-			name: user.sender.name,
-			image: user.profile_image
-		},
-		message: `
+		}
+		scrollToBottom();
+		updatelastchat(user);
+		socket.emit("send-chat", {
+			sender: {
+				name: user.sender.name,
+				image: user.profile_image
+			},
+			message: `
 		<iframe id="youtube" src="${youtube_upload.value}" allowfullscreen></iframe>
 		`
-	})
-	console.log(youtube_upload.value);
+		});
+		console.log(youtube_upload.value);
+	}
+}
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log("Name: " + profile.getName());
+	console.log("Image URL: " + profile.getImageUrl());
+	console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function() {
+		console.log("User signed out.");
+	});
+}
